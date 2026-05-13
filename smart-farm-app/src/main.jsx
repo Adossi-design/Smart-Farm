1cfd23dbd9b209efc70f1ddfff1de167daebcb8f
Adossi Fred William <f.adossi@alustudent.com>
2026-05-13 20:56:45 +0200

    React application entrypoint wiring the root provider stack into the DOM

--- smart-farm-app/src/main.jsx
+++ smart-farm-app/src/main.jsx
@@ -5,14 +5,20 @@ import './index.css'
 import './i18n';
 import { AuthProvider } from './context/AuthContext';
 import { DataProvider } from './context/DataContext';
-import 'react-quill/dist/quill.snow.css'; // Import Quill styles
+import { ThemeProvider } from './context/ThemeContext';
+import { ToastProvider } from './context/ToastContext';
+import 'react-quill/dist/quill.snow.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <DataProvider>
-        <App />
-      </DataProvider>
-    </AuthProvider>
-  </React.StrictMode>,
-)
+    <ToastProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <DataProvider>
+            <App />
+          </DataProvider>
+        </ThemeProvider>
+      </AuthProvider>
+    </ToastProvider>
+  </React.StrictMode>
+);
